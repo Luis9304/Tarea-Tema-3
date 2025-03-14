0da114056242c21eaa2a3a3c08d7f0763381a8de
Luis9304 <luis_valadez86@hotmail.com>
2025-03-14 01:49:34 -0600

    Programa 1

---
+++ AreadelRectangulo/src/areadelrectangulo/AreadelRectangulo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areadelrectangulo;
+
+/**
+ *
+ * @author luis_
+ */
+public class AreadelRectangulo {
+    
+       /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int area_rectangulo = 0;
+        int base = 10;
+        int altura = 20;
+        area_rectangulo = base*altura;
+        System.out.println("El area de un rectangulo de base "+base+" y altura "+altura+" es igual a "+area_rectangulo);
+        
+                
+                
+        
+        
+    }
+    
+}
